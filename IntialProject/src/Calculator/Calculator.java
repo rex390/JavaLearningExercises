fcbf2ccbccf91fc8e85131f29013785b7cc826ce
Mohammed Almas <deriftslayer@yahoo.co.uk>
2019-01-23 16:26:04 +0000

    the calculator with scanner input

--- IntialProject/src/Calculator/Calculator.java
+++ IntialProject/src/Calculator/Calculator.java
@@ -1,4 +1,7 @@
 package Calculator;
+
+import java.util.ArrayList;
+
 public class Calculator 
 {
 	
@@ -19,41 +22,41 @@ public class Calculator
 		return (intArg - intArg2);
 	}
 	
-	public void checkRelation(float... numberPassed)
+	public void checkRelation(ArrayList<Float> numberPassed)
 	{
-		for(int numberPicked1 = 0 ; numberPicked1 < numberPassed.length ; numberPicked1++)
+		for(int numberPicked1 = 0 ; numberPicked1 < numberPassed.size() ; numberPicked1++)
 		{
-			for(int numberPicked2 = 0 ; numberPicked2 < numberPassed.length ; numberPicked2++)
+			for(int numberPicked2 = 0 ; numberPicked2 < numberPassed.size() ; numberPicked2++)
 			{				
 				if(numberPicked1 != numberPicked2)
 				{
-					float sumAdd = add(numberPassed[numberPicked1],numberPassed[numberPicked2]);
- 					float sumSub = subtract(numberPassed[numberPicked1],numberPassed[numberPicked2]);
- 					float sumDiv = divide(numberPassed[numberPicked1],numberPassed[numberPicked2]);
- 					float sumMul = multiply(numberPassed[numberPicked1],numberPassed[numberPicked2]);
-					for(int findLast = 0 ; findLast <numberPassed.length;findLast++)
+					float sumAdd = add(numberPassed.get(numberPicked1),numberPassed.get(numberPicked2));
+ 					float sumSub = subtract(numberPassed.get(numberPicked1),numberPassed.get(numberPicked2));
+ 					float sumDiv = divide(numberPassed.get(numberPicked1),numberPassed.get(numberPicked2));
+ 					float sumMul = multiply(numberPassed.get(numberPicked1),numberPassed.get(numberPicked2));
+					for(int findLast = 0 ; findLast <numberPassed.size();findLast++)
 					{
 						if(findLast != numberPicked1 & findLast != numberPicked2)
 						{
-							if(sumAdd == numberPassed[findLast])
+							if(sumAdd == numberPassed.get(findLast))
 							{
-								System.out.println(numberPassed[numberPicked1] + " + " + numberPassed[numberPicked2] 
-										+  " = " + numberPassed[findLast]);
+								System.out.println(numberPassed.get(numberPicked1) + " + " + numberPassed.get(numberPicked2) 
+										+  " = " + numberPassed.get(findLast));
 							}
-							if(sumSub == numberPassed[findLast])
+							if(sumSub == numberPassed.get(findLast))
 							{
-								System.out.println(numberPassed[numberPicked1] + " - " + numberPassed[numberPicked2] 
-										+  " = " + numberPassed[findLast]);
+								System.out.println(numberPassed.get(numberPicked1) + " - " + numberPassed.get(numberPicked2) 
+								+  " = " + numberPassed.get(findLast));
 							}
-							if(sumDiv == numberPassed[findLast])
+							if(sumDiv == numberPassed.get(findLast))
 							{
-								System.out.println(numberPassed[numberPicked1] + " / " + numberPassed[numberPicked2] 
-										+  " = " + numberPassed[findLast]);
+								System.out.println(numberPassed.get(numberPicked1) + " / " + numberPassed.get(numberPicked2) 
+								+  " = " + numberPassed.get(findLast));
 							}
-							if(sumMul == numberPassed[findLast])
+							if(sumMul == numberPassed.get(findLast))
 							{
-								System.out.println(numberPassed[numberPicked1] + " * " + numberPassed[numberPicked2] 
-										+  " = " + numberPassed[findLast]);
+								System.out.println(numberPassed.get(numberPicked1) + " * " + numberPassed.get(numberPicked2) 
+								+  " = " + numberPassed.get(findLast));
 							}
 						}
 					}
--- IntialProject/src/Calculator/CalculatorMain.java
+++ IntialProject/src/Calculator/CalculatorMain.java
@@ -1,13 +1,45 @@
 package Calculator;
 
-public class CalculatorMain {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class CalculatorMain 
+{
 	public static void main(String[] args) 
 	{
-		Calculator calc = new Calculator();
-		calc.checkRelation(5, 3, 15);
+		Calculator calc = new Calculator();	
+		ArrayList<Float> floatList = attempt();
+		calc.checkRelation(floatList);
+	}	
 
+	public static ArrayList attempt()
+	{
+		Scanner inputScanner = new Scanner(System.in);
+		boolean badInput = false;
+		ArrayList<Float> floatList = new ArrayList<Float>();
+		do
+		{
+			System.out.println("input your numbers");			
+
+			String s = inputScanner.nextLine();
+			String[] chars = s.split(" ");
+			try 
+			{
+				for(String character:chars)
+				{
+					float i = Float.parseFloat(character);
+					floatList.add(i);	
+					badInput = false;
+				}
+		
+			}	
+			catch (Exception ex) 
+			{
+				badInput = true;
+			} 
+
+		} while (badInput);
+		
+		return floatList;
 	}
-
-	
 }
